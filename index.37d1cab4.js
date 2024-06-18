27c95aa83f8482ce815f7b670681a8da61acc429
Vyacheslav Kokayko <vkokayko@gmail.com>
2024-06-18 15:53:40 +0300

    make build

---
+++ index.37d1cab4.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table").rows,r=0;r<e.length;r++){var l=e[r].cells;if(l.length>1){var o=l[1].cloneNode(!0);e[r].insertBefore(o,l[l.length-1])}}
+//# sourceMappingURL=index.37d1cab4.js.map
